6404242ce6db7a9be6a0f9a074c896fb23353a35
jojoeiei <143501699+jojoeiei@users.noreply.github.com>
2023-09-08 11:34:06 +0700

    Delete cs211-661-project-am-ft-ch-mo directory

--- cs211-661-project-am-ft-ch-mo/src/main/java/cs211/project/models/Admin.java
+++
@@ -1,8 +0,0 @@
-//package cs211.project.models; //todo : make
-//
-//public class Admin extends User{
-//
-//    public Admin(String id, String name, String password, String rank) {
-//        super(id, name, password, rank);
-//    }
-//}
--- cs211-661-project-am-ft-ch-mo/src/main/java/cs211/project/models/AdminList.java
+++
@@ -1,4 +0,0 @@
-package cs211.project.models;
-
-public class AdminList {
-}
